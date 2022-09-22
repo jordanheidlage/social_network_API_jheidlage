50cfdee23c144649396f95539d79bfb1133b2bd8
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-09-22 16:27:07 -0400

    completed reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 // const moment = require('moment');
 const reactionSchema = new mongoose.Schema({
-
-})
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => timeSince(date)
+    }
+},
+{
+    timestamps: true,
+    toJSON: {getters: true, virtuals: true }
+}
+);
 
 
 
