fafc76a256f66015655017654a04133279104818
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-09-27 11:23:50 -0400

    add friend/delete friend routes completed

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,12 +6,14 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  deleteFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friends/:friendId').post().delete();
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
