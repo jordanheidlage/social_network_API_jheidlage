1bb10fabcee21bffec13fcf2d6f2e61c0fc80318
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-09-27 13:47:54 -0400

    completed thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
 
-const {
-
+const { getThoughts, 
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
 } = require ('../../controllers/thoughtController');
 
-router.route('/').get().post();
+router.route('/').get(getThoughts).post(createThought);
 
-router.route('/:thoughtId').get().put().delete();
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-router.route('/:thoughtId/reactions').post();
+router.route('/:thoughtId/reactions').post(createReaction);
 
-router.route('/:thoughtId/reactions/:reactionId').delete();
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 module.exports = router;
